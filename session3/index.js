347c4ace7706222bc1baa9d4db1124fbe68ae112
BAER12tumo <baer12@tumo.world>
2023-01-14 11:08:49 +0100

    gol über express

--- session3/index.js
+++ session3/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.use("/gol", express.static("../Game of Live"))
 
 app.get("/", function(req, res){
     res.send("<h2>Meine Überschrift</h2>");
@@ -14,3 +15,16 @@ app.get("/name/:name", function(req, res){
     let name = req.params.name;
     res.send("<h1>Hello " + name +"</h1>");
  });
+
+ app.get("/google", function(req, res){
+     res.redirect("http://google.com")
+ })
+
+ app.get("/google/:search", function(req, res){
+     let search = req.params.search;
+     res.redirect("http://google.com/search?q=" + search)
+ }) 
+ app.get("/*", function(req, res){
+     res.status(404).send("404 not found")
+ })
+
